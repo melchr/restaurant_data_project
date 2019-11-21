cee8eab8c90bd5973fb0617f838fa58fc552c1f5
Mel <melchrstmn@gmail.com>
2019-11-21 13:40:29 -0500

    connected all files required for environment

---
+++ lib/environment.rb
@@ -0,0 +1,7 @@
+require 'pry'
+require 'httparty'
+require_relative "restaurant_data_project/version"
+require_relative "restaurant_data_project/cli"
+require_relative "restaurant_data_project/opendata_api"
+require_relative "restaurant_data_project/restaurant_violations"
+
