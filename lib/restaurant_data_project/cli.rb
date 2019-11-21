59ea642c5c405b433934277d6331a106e3e30245
Mel <melchrstmn@gmail.com>
2019-11-21 13:44:23 -0500

    created cli file, defined method to get data from api

---
+++ lib/restaurant_data_project/cli.rb
@@ -0,0 +1,9 @@
+class CLI
+    def begin
+        OpenDataAPI.get_data
+    end
+    #methods to welcome user and display list using restaurant names & violations
+
+    #RestaurantViolations.all
+
+end
