af1da6976f0e4075f7279d3f594bdf1e1af48a4b
Mel <melchrstmn@gmail.com>
2019-11-26 12:48:16 -0500

    changed name from violation to inspection in all files

--- lib/restaurant_data_project/cli.rb
+++ lib/restaurant_data_project/cli.rb
@@ -4,13 +4,15 @@ class CLI
     end
 
     def start
-        puts "Welcome to the Restaurant Violation CLI!"
+        puts "Welcome to the Restaurant Violation CLI! Would you like to search for a Restaurant or a Grade Rating?"
+        input = gets.chomp
     end
 
     #methods to welcome user and display list using restaurant names & violations
 
-    #give user a list of restaurants to choose from (will be 25 options)
-    #or could I give user an option to search by borough?
+    #give user a list of restaurants to choose from (will be 25 #]options)
+    #search by rating (B, C), maybe should just do B & C
+    #and say "If your restaurant is not listed here, then it has an A rating!"
 
     #RestaurantViolations.all
 
