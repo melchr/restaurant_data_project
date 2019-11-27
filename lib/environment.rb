1fe684a8b1d3f7a5ca7737aa1c7b469cabca128d
Mel <melchrstmn@gmail.com>
2019-11-26 19:56:23 -0500

    corrected file in environment

--- lib/environment.rb
+++ lib/environment.rb
@@ -3,5 +3,4 @@ require 'httparty'
 require_relative "restaurant_data_project/version"
 require_relative "restaurant_data_project/cli"
 require_relative "restaurant_data_project/opendata_api"
-require_relative "restaurant_data_project/restaurant_violations"
-
+require_relative "restaurant_data_project/restaurant_inspections"
