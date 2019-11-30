e998fc907fa6eb8e6ea338fb203661a623a7519c
Mel <melchrstmn@gmail.com>
2019-11-30 00:05:40 -0500

    added new method for single data

--- lib/restaurant_data_project/opendata_api.rb
+++ lib/restaurant_data_project/opendata_api.rb
@@ -2,7 +2,13 @@ class OpenDataAPI
     def self.get_data
         response = HTTParty.get('https://data.cityofnewyork.us/resource/43nn-pn8j.json')
         response[0..24].each do |hash| 
-            RestaurantViolations.new(hash)
+            RestaurantInspections.new(hash)
+        end
+    end
+    def self.single_data(restaurant)
+        response = HTTParty.get('https://data.cityofnewyork.us/resource/43nn-pn8j.json')
+        response[0..24].each do |hash| 
+            RestaurantInspections.new(hash)
         end
     end
 end
