c43ae67c0ea023d30f005da99a22c5c6f92cc66d
Mel <melchrstmn@gmail.com>
2019-12-04 12:01:54 -0500

    cleaned up cli class

--- lib/restaurant_data_project/cli.rb
+++ lib/restaurant_data_project/cli.rb
@@ -8,44 +8,43 @@ class CLI
     def menu
         input = nil
           puts "Type 1 for a list of Restaurants, type Exit to leave the program at anytime."
-          while input != "exit"
-          input = gets.strip.downcase
-            if input == "1"
-              RestaurantInspections.all.each.with_index do |restaurant, index|
-              puts "#{index + 1}: #{restaurant.restaurant_name}"
-              end
-               more_info
-            elsif input == "exit"
-                puts "\nThanks for using the Restaurant Inspection CLI!"
-                puts "\n"
-                exit
-            else
-              puts "Invalid entry, please type either 1 or exit."
-           end
-          end
+            while input != "exit"
+                input = gets.strip.downcase
+                if input == "1"
+                    RestaurantInspections.all.each.with_index do |restaurant, index|
+                    puts "#{index + 1}: #{restaurant.restaurant_name}"
+                end
+                more_info
+                elsif input == "exit"
+                    puts "\nThanks for using the Restaurant Inspection CLI!"
+                    puts "\n"
+                    exit
+                else
+                    puts "Invalid entry, please type either 1 or exit."
+                end
+            end
     end
 
-    def more_info #if i type "exit", the program continues, if i type anything else, the program continues
+    def more_info
         input = nil
-        puts "\nPlease type the number of the restaurant you'd like to learn more about, or type 'exit' to leave the program."
-        while input != "exit"
-        input = gets.chomp
-        if input.to_i.between?(1,26) #dont hard code 25, data type for gets.chomp
-        restaurant = RestaurantInspections.all[input.to_i - 1]
-        OpenDataAPI.single_data(restaurant)
-        puts "\nRestaurant Name: #{restaurant.restaurant_name}"
-        puts "Type of Food: #{restaurant.type_of_food}"
-        puts "Grade Rating: #{restaurant.grade}"
-        puts "Violation Description: #{restaurant.violation_description}"
-        diff_restaurant
-        elsif input == "exit"
-            puts "Thanks for using the Restaurant Inspection CLI!"
-            exit
-        else
-            puts "Invalid entry, please type the number of the restaurant you'd like to learn about, or type 'exit'."
-            #more_info
-        end
-        end
+          puts "\nPlease type the number of the restaurant you'd like to learn more about, or type 'exit' to leave the program."
+            while input != "exit"
+            input = gets.chomp
+            if input.to_i.between?(1,25) #dont hard code 25, data type for gets.chomp
+                restaurant = RestaurantInspections.all[input.to_i - 1]
+                OpenDataAPI.single_data(restaurant)
+                puts "\nRestaurant Name: #{restaurant.restaurant_name}"
+                puts "Type of Food: #{restaurant.type_of_food}"
+                puts "Grade Rating: #{restaurant.grade}"
+                puts "Violation Description: #{restaurant.violation_description}"
+            diff_restaurant
+            elsif input == "exit"
+              puts "Thanks for using the Restaurant Inspection CLI!"
+              exit
+            else
+              puts "Invalid entry, please type the number of the restaurant you'd like to learn about, or type 'exit'."
+            end
+            end
     end
 
         def diff_restaurant
@@ -71,14 +70,4 @@ class CLI
                end
               end
         end
-        
-        #else "What Restaurant would you like to search for?"
-        #create method for each to display lists^^^^^
-
-    #should not terminate unless user types EXIT, should be aware they can type exit whenever
-    #input validation, re-ask for input, say invalid input using if else
-
 end
-
-
-
